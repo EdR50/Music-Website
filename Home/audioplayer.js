9bbca715556896fdbf22ad8379333f0987a47587
edR50 <narutogr7@gmail.com>
2020-06-24 15:28:02 -0300

    Solucionado bug de mostrar imagenes en el audioplayer

--- Home/audioplayer.js
+++ Home/audioplayer.js
@@ -9,8 +9,8 @@ const xsong = document.getElementById("Xsong")
 //Declarando variables para el reproductor
 let song = new Audio()
 let count = 0
-let playlist = ["./music/audio.mp3", "./music/audio2.mp3", "./music/4u.mp3"]
-let covers = ["img/album.jpg"]
+let playlist = ["./music/audio.mp3","./music/4u.mp3" , "./music/audio2.mp3" ,  "./music/K.mp3"]
+let covers = ["img/album.jpg", "./img/img.jpg", "", "./img/Cigarettes_After_Sex.png"]
 song.src = playlist[count]
 
 // FUNCIONES
